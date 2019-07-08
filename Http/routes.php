9ea69897dc41c1b528e97675e55a5bd7b3e7e6ff
zeloras <zeloras@gmail.com>
2019-07-08 19:54:42 +0300

    some fixes for modules

--- Http/routes.php
+++ Http/routes.php
@@ -2,18 +2,18 @@
 
 Route::group(['middleware' => ['web', 'permission:' . \Gcms::MAIN_ADMIN_PERMISSION], 'prefix' => getAdminPrefix('seo')], function () {
     Route::group(['middleware' => ['permission:modules_seo_admin_list']], function () {
-        Route::get('/', function () {
+        Route::get(DIRECTORY_SEPARATOR, function () {
         });
     });
 
     Route::group(['middleware' => ['permission:modules_seo_admin_list']], function () {
         // redirect
         Route::group(['prefix' => 'redirect'], function () {
-            Route::get('/', function () {
+            Route::get(DIRECTORY_SEPARATOR, function () {
                 return 'redirect get';
             })->name('admin.seo.redirect');
 
-            Route::post('/', function () {
+            Route::post(DIRECTORY_SEPARATOR, function () {
                 return 'redirect post';
             });
 
